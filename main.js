ab11d84ea7e7fb2734de89066cc59ad0ece05a00
tee5 <noppe18@gmail.com>
2020-06-18 14:52:34 +0900

    add light on button

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    document.querySelector("#bedroomLightNightButton").addEventListener("click", onclickBedroomLightNightButton);
+    document.querySelector("#bedroomLightOnButton").addEventListener("click", onclickBedroomLightOnButton);
     document.querySelector("#bedroomLightOffButton").addEventListener("click", onclickBedroomLightOffButton);
+    document.querySelector("#bedroomLightNightButton").addEventListener("click", onclickBedroomLightNightButton);
 }
 
 function onclickBedroomLightNightButton() {
@@ -19,6 +20,17 @@ function onclickBedroomLightNightButton() {
 
 function onclickBedroomLightOffButton() {
     const url = "https://maker.ifttt.com/trigger/BEDROOM_LIGHT_OFF/with/key/dHtrMPsLT6qsQQWOK-VAr4";
+    fetch(url)
+        .then(function (respons) {
+            return respons.json();
+        })
+        .then(function (json) {
+            console.log(JSON.stringify(json));
+        })
+}
+
+function onclickBedroomLightOnButton() {
+    const url = "https://maker.ifttt.com/trigger/BEDROOM_LIGHT_ON/with/key/dHtrMPsLT6qsQQWOK-VAr4";
     fetch(url)
         .then(function (respons) {
             return respons.json();
